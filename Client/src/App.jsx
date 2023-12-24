83374c82b34bb6acd7d56d60212888f55c128942
alisiddhamza <alihamza6871@gmail.com>
2023-12-25 01:06:55 +0530

    React-routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Policy from './pages/Policy'
+import PageNotFound from './pages/PageNotFound'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Ecommerce App</h1>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/policy" element={<Policy/>}/>
+      <Route path="/*" element={<PageNotFound/>}/>
+    </Routes>
     </>
   )
 }
