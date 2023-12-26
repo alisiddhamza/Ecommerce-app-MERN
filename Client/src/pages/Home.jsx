2b538f2f50774337e9e2ddc1e4962669dfb11ead
alisiddhamza <alihamza6871@gmail.com>
2023-12-26 17:33:47 +0530

    context_api_logout

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
-
+import { useAuth } from "../Context/authContext.jsx";
 function Home() {
+  const [auth, setAuth] = useAuth();
   return (
-    <Layout>
-        <h1>Home Page</h1>
+    <Layout title={"Best offers "}>
+      <h1>HomePage</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
   )
 }
