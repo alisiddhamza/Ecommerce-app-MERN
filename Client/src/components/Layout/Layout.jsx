ee87aefc147e5d41d8fe7416f9845ce414de7387
alisiddhamza <alihamza6871@gmail.com>
2023-12-26 13:28:22 +0530

    Login_Register_setup

--- Client/src/components/Layout/Layout.jsx
+++ Client/src/components/Layout/Layout.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-const Layout = ({children}) => {
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Toaster } from "react-hot-toast";
+import { Helmet } from "react-helmet";
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <>
-    <Header/>
-    <main style={{minHeight:"72vh"}}>
-    {children}
-    </main>
-    <Footer/>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "72vh" }}>
+        <Toaster />
+        {children}
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "Ali Hamza",
+};
+
+export default Layout;
--- server.js
+++ server.js
@@ -3,14 +3,18 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js'
+import cors from "cors";
 const app= express();
 const PORT=process.env.PORT || 8080;
+
+
 dotenv.config();
 
 // datase connection
 connectDB();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
