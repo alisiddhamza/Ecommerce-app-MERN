9a6343d1613c5fa84d0d63ccd40bed9b8347947c
alisiddhamza <alihamza6871@gmail.com>
2023-12-28 14:41:40 +0530

    dashboard_setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,28 +1,31 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Policy from './pages/Policy'
-import PageNotFound from './pages/PageNotFound'
-import Register from './pages/Auth/Register'
-import Login from './pages/Auth/Login'
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/User/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/policy" element={<Policy/>}/>
-      <Route path="/*" element={<PageNotFound/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
