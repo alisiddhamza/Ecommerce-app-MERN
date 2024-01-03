85eaa45566d5f62120497637c36ab5ad43e524b1
alisiddhamza <alihamza6871@gmail.com>
2024-01-03 17:16:52 +0530

    UserProfile_setup

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,12 @@
 import express from 'express';
 const router=express.Router();
-import {registerController, loginController , testController, forgotPasswordController} from '../controllers/authController.js'
+import {
+  registerController, 
+  loginController , 
+  testController, 
+  forgotPasswordController, 
+  updateProfileController
+} from '../controllers/authController.js'
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'; 
 
 //register method post
@@ -24,5 +30,8 @@ router.get("/user-auth", requireSignIn, (req, res) => {
 router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
 
 export default router;
