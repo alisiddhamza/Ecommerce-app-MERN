6aec1e6916be67f1e7191386fcaeb06c0d3ba55a
Ali Hamza <alihamza6871@gmail.com>
2024-01-05 16:05:38 +0530

    production_setup

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/authRoute.js'
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from 'path';
 
 const app= express();
 const PORT=process.env.PORT || 8080;
@@ -20,6 +21,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './Client/dist')));
 
 //routes
 app.use('/api/v1/auth',authRoutes);
@@ -27,10 +29,14 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get('/',(req,res)=>{
-    res.send("<h1>Welcome to ecomerce app</h1>")
+// app.get('/',(req,res)=>{
+//     res.send("<h1>Welcome to ecomerce app</h1>")
 
-} )
+// } )
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./Client/dist/index.html'));
+})
+
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
